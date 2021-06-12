849163e4b74f1028032c9068c4cb6751c530c54c
RenataFebrianaJacom <renatafebrianajacom@gmal.com>
2021-06-12 15:35:14 +0800

    Membuat Variable

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Renata";
+        String namaTengah = "Febriana";
+        String namaBelakang ="Jacom";
+
+        int usia = 20;
+        int targetTahunKuliah =4;
+        double ipk = 3.999;
+        char nilaiAbjad ='A';
+        boolean Cantik = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
 }
