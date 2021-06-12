466b74dc5437a503ae85c84e4bff20229ca4a040
RenataFebrianaJacom <renatafebrianajacom@gmal.com>
2021-06-12 16:17:47 +0800

    Operasi Tipe String

---
+++ src/Model/MahasiswaBeraksi.java
@@ -0,0 +1,12 @@
+package Model;
+
+import java.util.Date;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.npm = "19630453";
+        mahasiswa.nama ="Renata Febriana Jacom";
+        mahasiswa.tanggalLahir = new Date();
+    }
+    }
