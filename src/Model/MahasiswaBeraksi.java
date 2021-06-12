ca610f76e35aa82741615bc9ce0846bd07085961
RenataFebrianaJacom <renatafebrianajacom@gmal.com>
2021-06-12 16:29:31 +0800

    membuat class,atribut,dan objek

--- src/Model/MahasiswaBeraksi.java
+++ src/Model/MahasiswaBeraksi.java
@@ -1,12 +1,32 @@
 package Model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new  SimpleDateFormat(polaTanggal);
+
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19630453";
         mahasiswa.nama ="Renata Febriana Jacom";
-        mahasiswa.tanggalLahir = new Date();
+        mahasiswa.tanggalLahir = simpleDateFormat.parse(" 21-02-2001");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa.nama);
+        System.out.println("NPM \t\t\t  : " + mahasiswa.npm);
+        System.out.println("tanggalLahir \t : " + mahasiswa.tanggalLahir);
+
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        mahasiswa2.npm = "1963045";
+        mahasiswa2.nama ="Renata Jacom";
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse(" 22-02-2002");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+        System.out.println("NPM \t\t\t  : " + mahasiswa2.npm);
+        System.out.println("tanggalLahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
+
     }
     }
